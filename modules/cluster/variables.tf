2a2e96b27a439600bb8233a094b08f490b658940
eeronjohn <76084575+eeronjohn@users.noreply.github.com>
2021-02-26 10:47:37 +0800

    Add files via upload

---
+++ modules/cluster/variables.tf
@@ -0,0 +1,15 @@
+variable "serviceprinciple_id" {
+
+}
+variable "serviceprinciple_key" {
+
+}
+variable "location" {
+    default =  "southeastasia"
+}
+variable "kubernetes_version" {
+    default = "1.17.13"
+}
+variable "ssh_key" {
+  
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "serviceprinciple_id" {
+}
+variable "serviceprinciple_key" {
+}
+variable "tenant_id" {
+}
+variable "subscription_id" {    
+}
+variable "location" {
+    default =  "southeastasia"
+}
+variable "kubernetes_version" {
+    default = "1.17.13"
+}
+variable "ssh_key" {
+  
+}
